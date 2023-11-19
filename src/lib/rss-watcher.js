ea0da39486c396cb1478011fe9002228519e4ec5
Mitsutoshi Nakano <ItSANgo@gmail.com>
2023-11-19 10:34:52 +0900

    fefactor

--- src/lib/rss-watcher.js
+++ src/lib/rss-watcher.js
@@ -1,30 +1,27 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 'use strict'
-
-const df = require('durable-functions')
 const Parser = require('rss-parser')
+const df = require('durable-functions')
 const entityName = 'saver'
-const send = require('./send-line.js')
 
 const parser = new Parser()
 
 function differ (current, previous, context) {
   if (previous == null) {
-    // context.log('previous == null')
+    context.log('previous == null')
     return current
   }
   if (current == null) {
-    // context.warn('illegal current format')
+    context.warn('illegal current format')
     return []
   }
+  context.log(`current: ${current.length}, previous: ${previous.length}`)
   const map = new Map()
   current.forEach((item) => { map.set(item.link, item) })
-  if (previous != null) {
-    previous.forEach((item) => { map.delete(item.link) })
-  }
+  previous.forEach((item) => { map.delete(item.link) })
   const diff = []
   map.forEach((item) => { diff.push(item) })
-  // context.log(`${diff.length} differs`)
+  context.log(`${diff.length} differs`)
   return diff
 }
 
@@ -51,17 +48,24 @@ module.exports = async (urls, myTimer, context) => {
   }
   // context.log({ current })
 
-  const previous = client.readEntityState(entityId).entityState
-  context.log({ previous })
+  const response = await client.readEntityState(entityId)
+  // context.log({ response })
+  const previous = response.entityState
+  // context.log({ previous })
   const diff = differ(current, previous, context)
   // context.log({ diff })
   if (diff.length < 1) {
     context.log('return void')
     return
   }
-  client.signalEntity(entityId, 'post', current)
+  await client.signalEntity(entityId, 'post', current)
   const text = head(5000, diff.map(i => `${i.link} ${i.title}`)).join('\r\n')
   const length = text.length
-  context.log({ text, length })
-  return await send(context, text)
+  context.log({ length })
+  try {
+    return await require('./send-line')(text)
+  } catch (err) {
+    context.error(err)
+    throw err
+  }
 }
--- src/lib/send-line.js
+++ src/lib/send-line.js
@@ -4,15 +4,8 @@
  * @see https://github.com/line/line-bot-sdk-nodejs/blob/master/docs/guide/client.md
  */
 const MessagingApiClient = require('@line/bot-sdk').messagingApi.MessagingApiClient
-
-module.exports = async (context, text) => {
-  const lineClient = new MessagingApiClient({
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN
-  })
-  return lineClient.pushMessage(process.env.LINE_ID, {
-    type: 'text', text
-  }).catch(err => {
-    context.error(err)
-    throw err
-  })
-}
+module.exports = async (text) => new MessagingApiClient({
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN
+}).pushMessage({
+  to: process.env.LINE_ID, messages: [{ type: 'text', text }]
+})
