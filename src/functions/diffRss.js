eaf9f0c1f0243c71083fb1c98e6086f9f3eba046
Mitsutoshi Nakano <ItSANgo@gmail.com>
2024-01-03 14:27:24 +0900

    add LINE 429 error checker

--- src/functions/diffRss.js
+++ src/functions/diffRss.js
@@ -5,35 +5,19 @@
  */
 'use strict'
 const { app } = require('@azure/functions')
-const df = require('durable-functions')
+const { durableClient, entity, keys } = require('../lib/entity')
+const canUseLine = require('../lib/can-use-line')
 const watch = require('../lib/rss-watcher')
-const entityName = 'saver'
-
-/** Get the entity contents by the key.
- * @param {*} key Durable Function's Entity Key
- * @param {*} context Azure Functions context object
- * @returns key contents  */
-async function entity (key, context) {
-  const client = df.getClient(context)
-  const entityId = new df.EntityId(entityName, key)
-  const response = await client.readEntityState(entityId)
-  return response.entityState
-}
 
 /** Registers diffRss Timer Function.  */
 app.timer('diffRss', {
   schedule: process.env.DIFFRSS_SCHEDULE,
-  extraInputs: [df.input.durableClient()],
+  extraInputs: [durableClient()],
   handler: async (myTimer, context) => {
-    /** The time when LINE Messageing API responsed HTTP 429 error.
-     * @type Date  */
-    const lastRateError = await entity('lastRateError', context)
-    if (lastRateError &&
-      Date.now() - lastRateError.getTime() <=
-      process.env.AZFUNBOT_WAIT_ERROR * 24 * 60 * 60 * 1000) { return }
+    if (!canUseLine(context)) { return }
     /** URLs list.
      * @type string[]  */
-    const urls = await entity('urls', context)
+    const urls = await entity(keys.urls, context)
     if (urls == null || urls.length < 1) { return }
     return watch(urls, myTimer, context)
   }
