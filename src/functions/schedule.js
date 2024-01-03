692572e291c0d20b4cc704dfcd2c06229a81cd2b
Mitsutoshi Nakano <ItSANgo@gmail.com>
2024-01-03 14:22:53 +0900

    modify the style

--- src/functions/schedule.js
+++ src/functions/schedule.js
@@ -5,32 +5,28 @@
  */
 'use strict'
 const { app } = require('@azure/functions')
-const df = require('durable-functions')
-const entityName = 'saver'
+const {
+  durableClient, getClient, newEntityId, entityState, postEntity, keys
+} = require('../lib/entity')
 
 /** Web Function.  */
 app.http('schedule', {
   methods: ['GET', 'POST'],
   authLevel: 'function',
-  extraInputs: [df.input.durableClient()],
+  extraInputs: [durableClient()],
   handler: async (request, context) => {
-    context.log(`${request.method} ${request.url}`)
-    const client = df.getClient(context)
     context.log({ request })
-    context.log({ request_query: request.query })
-    const key = request.query.get('key') || 'schedule'
-    context.log({ query_key: request.query.get('key') })
+    const key = request.query.get('key') || keys.schedule
     context.log({ key })
-    const entityId = new df.EntityId(entityName, key)
+    const client = getClient(context)
+    const entityId = newEntityId(key)
     switch (request.method) {
-      case 'GET': {
-        const response = await client.readEntityState(entityId)
-        const schedule = response.entityState
-        return { body: JSON.stringify(schedule) }
-      }
+      case 'GET':
+        return { body: JSON.stringify(await entityState(client, entityId)) }
       case 'POST': {
         const schedule = await request.json()
-        await client.signalEntity(entityId, 'post', schedule)
+        context.log({ schedule })
+        await postEntity(schedule, entityId, client)
         return { body: 'accept' }
       }
     }
