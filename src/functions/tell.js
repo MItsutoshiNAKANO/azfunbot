ac82fba9e3e6a2a8b3b9c39d5de6bbb952cd75c8
Mitsutoshi Nakano <ItSANgo@gmail.com>
2024-04-07 22:49:16 +0900

    update tell.js

--- src/functions/tell.js
+++ src/functions/tell.js
@@ -8,6 +8,7 @@ const { app } = require('@azure/functions')
 const {
   durableClient, getClient, newEntityId, entityState, postEntity, keys
 } = require('../lib/entity')
+const pickSchedules = require('../lib/pick-schedules')
 const canUseLine = require('../lib/can-use-line')
 const send = require('../lib/send-line')
 
@@ -23,14 +24,9 @@ app.timer('tell', {
       context.log('The schedule is null')
       return
     }
-    const now = new Date()
-    const text = []
-    const newSchedule = schedule.map((i) => {
-      const times = i.times.filter((t) => Date.parse(t) > now)
-      if (times.length >= i.times.length) { return i }
-      text.push(i.text)
-      return { times, text: i.text }
-    })
+    const {
+      text, newSchedule
+    } = pickSchedules(schedule, new Date(), process.env.TELL_LOCALE)
     if (text.length < 1) {
       context.log('The schedule is 0')
       return
