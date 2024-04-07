a955c660ce2330b304570d38683224b08e931cc6
Mitsutoshi Nakano <ItSANgo@gmail.com>
2024-04-07 12:32:54 +0900

    modify paths

--- manual-test/test-mail-sender.js
+++ manual-test/test-mail-sender.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-const sendMail = require('../lib/mail-sender')
+const sendMail = require('../src/lib/mail-sender')
 try {
   sendMail(console, process.env.AZFUNBOT_MAIL_TO_ADMIN, 'test', 'テスト')
 } catch (err) { console.error(err) }
--- manual-test/test-mail-sender.sh
+++ manual-test/test-mail-sender.sh
@@ -2,6 +2,6 @@
 
 dir=$(dirname "$0")
 cd "$dir"
-source "../../secrets/test-mail-sender.rc.sh"
+source "../secrets/test-mail-sender.rc.sh"
 
 node "./test-mail-sender.js" "$@"
