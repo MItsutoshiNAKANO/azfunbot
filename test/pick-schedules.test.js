6ae888fb230ca595c86b4f43eb870139120c5908
Mitsutoshi Nakano <ItSANgo@gmail.com>
2024-04-13 09:49:44 +0900

    up the test date

--- test/pick-schedules.test.js
+++ test/pick-schedules.test.js
@@ -2,24 +2,24 @@ import { describe, expect, it } from 'vitest'
 const pickSchedules = require('../src/lib/pick-schedules')
 
 describe('pickSchedules', () => {
-  const now = new Date('2022-02-15 09:01')
+  const now = new Date('2024-04-13 09:01')
   const locale = 'ja-JP'
 
   it('should return today', () => {
     const result = pickSchedules([{
-      times: ['2022-02-15'], text: '#{today}'
+      times: ['2024-04-13'], text: '#{today}'
     }], now, locale)
     expect(result).toStrictEqual({
-      text: ['2022/2/15'], newSchedule: [{ times: [], text: '#{today}' }]
+      text: ['2024/4/13'], newSchedule: [{ times: [], text: '#{today}' }]
     })
   })
 
   it('should return tomorrow', () => {
     const result = pickSchedules([{
-      times: ['2022-02-15'], text: '#{tomorrow}'
+      times: ['2024-04-13'], text: '#{tomorrow}'
     }], now, locale)
     expect(result).toStrictEqual({
-      text: ['2022/2/16'], newSchedule: [{ times: [], text: '#{tomorrow}' }]
+      text: ['2024/4/14'], newSchedule: [{ times: [], text: '#{tomorrow}' }]
     })
   })
 })
