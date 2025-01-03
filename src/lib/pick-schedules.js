8497691355b09386640682f69c3e9c1fb474cc73
Mitsutoshi Nakano <ItSANgo@gmail.com>
2025-01-03 14:04:07 +0900

    Add aliases for dates

--- src/lib/pick-schedules.js
+++ src/lib/pick-schedules.js
@@ -16,10 +16,96 @@ const evaljson = require('evaljson')
  */
 function evalMacros (json, today, locale, options) {
   const tomorrow = new Date(
-    today.getFullYear(), today.getMonth(), today.getDate() + 1)
+    today.getFullYear(), today.getMonth(), today.getDate() + 1
+  )
+  const theDayAfterTomorrow = new Date(
+    today.getFullYear(), today.getMonth(), today.getDate() + 2
+  )
+  const in3Days = new Date(
+    today.getFullYear(), today.getMonth(), today.getDate() + 3
+  )
+  const in4Days = new Date(
+    today.getFullYear(), today.getMonth(), today.getDate() + 4
+  )
+  const in5Days = new Date(
+    today.getFullYear(), today.getMonth(), today.getDate() + 5
+  )
+  const in6Days = new Date(
+    today.getFullYear(), today.getMonth(), today.getDate() + 6
+  )
+  const inAWeek = new Date(
+    today.getFullYear(), today.getMonth(), today.getDate() + 7
+  )
+  const day = today.getDay()
+  const lastSunday = new Date(
+    today.getFullYear(), today.getMonth(), today.getDate() - day
+  )
+  const Sunday = new Date(
+    lastSunday.getFullYear(), lastSunday.getMonth(), lastSunday.getDate() + 7
+  )
+  const Monday = (day < 1
+    ? new Date(
+      lastSunday.getFullYear(), lastSunday.getMonth(),
+      lastSunday.getDate() + 1
+    )
+    : new Date(
+      Sunday.getFullYear(), Sunday.getMonth(),
+      Sunday.getDate() + 1)
+  )
+  const Tuesday = (day < 2
+    ? new Date(
+      lastSunday.getFullYear(), lastSunday.getMonth(),
+      lastSunday.getDate() + 2
+    )
+    : new Date(Sunday.getFullYear(), Sunday.getMonth(), Sunday.getDate() + 2)
+  )
+  const Wednesday = (day < 3
+    ? new Date(
+      lastSunday.getFullYear(), lastSunday.getMonth(),
+      lastSunday.getDate() + 3
+    )
+    : new Date(Sunday.getFullYear(), Sunday.getMonth(), Sunday.getDate() + 3)
+  )
+  const Thursday = (day < 4
+    ? new Date(
+      lastSunday.getFullYear(), lastSunday.getMonth(),
+      lastSunday.getDate() + 4
+    )
+    : new Date(Sunday.getFullYear(), Sunday.getMonth(), Sunday.getDate() + 4)
+  )
+  const Friday = (day < 5
+    ? new Date(
+      lastSunday.getFullYear(), lastSunday.getMonth(),
+      lastSunday.getDate() + 5
+    )
+    : new Date(Sunday.getFullYear(), Sunday.getMonth(), Sunday.getDate() + 5)
+  )
+  const Saturday = (day < 6
+    ? new Date(
+      lastSunday.getFullYear(), lastSunday.getMonth(),
+      lastSunday.getDate() + 6
+    )
+    : new Date(Sunday.getFullYear(), Sunday.getMonth(), Sunday.getDate() + 6)
+  )
   return evaljson(json, {
+    today: today.toLocaleDateString(locale, options),
     tomorrow: tomorrow.toLocaleDateString(locale, options),
-    today: today.toLocaleDateString(locale, options)
+    theDayAfterTomorrow: theDayAfterTomorrow.toLocaleDateString(
+      locale, options
+    ),
+    in3Days: in3Days.toLocaleDateString(locale, options),
+    in4Days: in4Days.toLocaleDateString(locale, options),
+    in5Days: in5Days.toLocaleDateString(locale, options),
+    in6Days: in6Days.toLocaleDateString(locale, options),
+    inAWeek: inAWeek.toLocaleDateString(locale, options),
+    lastSunday: lastSunday.toLocaleDateString(locale, options),
+    Sunday: Sunday.toLocaleDateString(locale, options),
+    Monday: Monday.toLocaleDateString(locale, options),
+    Tuesday: Tuesday.toLocaleDateString(locale, options),
+    Wednesday: Wednesday.toLocaleDateString(locale, options),
+    Thursday: Thursday.toLocaleDateString(locale, options),
+    Friday: Friday.toLocaleDateString(locale, options),
+    Saturday: Saturday.toLocaleDateString(locale, options)
   })
 }
 
