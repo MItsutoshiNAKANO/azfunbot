4c379276043e9405253b2af53b1f662c725dfe13
Mitsutoshi Nakano <ItSANgo@gmail.com>
2025-01-03 19:57:09 +0900

    Add help message into maintain script

--- scripts/maintain.mjs
+++ scripts/maintain.mjs
@@ -1,13 +1,18 @@
 #! /usr/bin/env node
 'use strict'
 /**
- * @file Maintain AzFunBot.
+ * @file
+ * @brief Maintain AzFunBot.
  * @license AGPL-3.0-or-later
  * SPDX-License-Identifier: AGPL-3.0-or-later
  * @description
- *   examples:
- *      ./scripts/maintain.mjs -D - -X POST --json @secrets/schedule.json 'localtest:?key=schedule'
- *      ./scripts/maintain.mjs -D - -X POST --json @secrets/schedule.json 'maintain:?code=&key=schedule'
+ *   Examples:
+ *     ./scripts/maintain.mjs -k -X GET 'localtest:?key=schedule'
+ *     ./scripts/maintain.mjs -k -X POST --json @secrets/schedule.json \
+ *       'localtest:?key=schedule'
+ *     ./scripts/maintain.mjs -k -X GET 'maintain:?code=&key=schedule'
+ *     ./scripts/maintain.mjs -k-X POST --json @secrets/schedule.json \
+ *       'maintain:?code=&key=schedule'
  */
 /** @see https://nodejs.org/docs/latest-v20.x/api/child_process.html  */
 import { spawn } from 'node:child_process'
@@ -23,10 +28,18 @@ const dirname = path.dirname(scriptPath)
 const basename = path.basename(scriptPath)
 const args = hideBin(process.argv)
 const curlArgs = args.filter((_val, index) => index < args.length - 1)
-const str = args[args.length - 1]
-const parsed = yargs([str]).config({
+const lastArg = args[args.length - 1]
+const parsed = yargs([lastArg]).config({
   extends: `${dirname}/../secrets/${basename}.config.json`
-}).argv
+}).usage(
+  `Usage: ${basename} [curl options] <url>
+Examples:
+
+${basename} -k -X GET 'localtest:?key=schedule'
+${basename} -k -X POST --json @secrets/schedule.json 'localtest:?key=schedule'
+${basename} -k -X GET 'maintain:?code=&key=schedule'
+${basename} -k -X POST --json @secrets/schedule.json\\
+\t'maintain:?code=&key=schedule'`).parse()
 const aliases = []
 for (const [key, value] of Object.entries(parsed)) {
   if (key !== '_' && key !== '$0') { aliases.push([key, value]) }
